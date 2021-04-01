133bc3457c16e12ba8e4ed2002432533023d7942
Sergio Sánchez <sss4esob@gmail.com>
2021-04-01 19:47:16 +0200

    Refactor location item UI

--- app/src/main/java/sanchez/sergio/kmp_test/ui/locations/LocationsListBindings.kt
+++ app/src/main/java/sanchez/sergio/kmp_test/ui/locations/LocationsListBindings.kt
@@ -1,9 +1,13 @@
 package sanchez.sergio.kmp_test.ui.locations
 
+import android.annotation.SuppressLint
 import android.view.View
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import sanchez.sergio.kmp_test.R
+import sanchez.sergio.kmp_test.domain.models.Location
 
 /**
  * Locations List Bindings
@@ -42,6 +46,60 @@ object LocationsListBindings {
         val adapter = view.adapter
         if(state is LocationsState.OnSuccess &&  adapter is LocationListAdapter)
             adapter.addData(state.pageData.data)
-
     }
+
+    /**
+     * Bind Location Created at
+     * @param view
+     * @param location
+     */
+    @SuppressLint("SetTextI18n")
+    @JvmStatic
+    @BindingAdapter("bindLocationCreatedAt")
+    fun bindLocationCreatedAt(view: TextView, location: Location) {
+        view.text = String.format(view.context.getString(R.string.location_created_at),
+            location.created)
+    }
+
+    /**
+     * Bind Location Type
+     * @param view
+     * @param location
+     */
+    @SuppressLint("SetTextI18n")
+    @JvmStatic
+    @BindingAdapter("bindLocationType")
+    fun bindLocationType(view: TextView, location: Location) {
+        view.text = String.format(view.context.getString(R.string.location_type),
+            location.type)
+    }
+
+
+    /**
+     * Bind Location Residents
+     * @param view
+     * @param location
+     */
+    @SuppressLint("SetTextI18n")
+    @JvmStatic
+    @BindingAdapter("bindLocationResidents")
+    fun bindLocationResidents(view: TextView, location: Location) {
+        view.text = String.format(view.context.getString(R.string.location_residents),
+            location.residents)
+    }
+
+    /**
+     * Bind Location Dimension
+     * @param view
+     * @param location
+     */
+    @SuppressLint("SetTextI18n")
+    @JvmStatic
+    @BindingAdapter("bindLocationDimension")
+    fun bindLocationDimension(view: TextView, location: Location) {
+        view.text = String.format(view.context.getString(R.string.location_dimension),
+            location.dimension)
+    }
+
+
 }
